1e57923c8bdceedc3708ed4651f8f8606e7389a8
Chiel92 <ctje92@gmail.com>
2013-11-22 23:50:03 +0100

    paste before and after implemented

--- clipboard_system/__init__.py
+++ clipboard_system/__init__.py
@@ -7,9 +7,18 @@ def copy(self):
     self.clipboard = self.content(self.selection)
 Session.copy = copy
 
-def paste(self):
+def paste(self, before=False):
     if self.clipboard:
-        operation = Operation(self, self.selection, self.clipboard)
+        content = self.content(self.selection)
+        new_content = []
+        l = len(self.clipboard)
+        for i, interval in enumerate(self.selection):
+            if before:
+                new_content.append(self.clipboard[i % l] + content[i])
+            else:
+                new_content.append(content[i] + self.clipboard[i % l])
+
+        operation = Operation(self, self.selection, new_content)
         self.apply(operation)
 Session.paste = paste
 
