43baf3157e6932ccd8bbcdf89d48f5507af7ab43
Chiel92 <ctje92@gmail.com>
2013-12-08 16:25:32 +0100

    user scripts can be loaded now

--- plugins.py
+++ plugins.py
@@ -1,8 +1,27 @@
 """This module loads all standard plugins."""
-from .session import Session
+from os.path import expanduser
+from sys import path
+import logging
+
+# Load standard plugins
 from . import filetype_system
 from . import undo_system
 from . import labeling_system
 
-import logging
-logging.info("All plugins are loaded.")
+logging.info('All standard plugins are loaded.')
+
+# Load user script
+path_to_user = expanduser('~') + '/.fate/'
+try:
+    path.insert(0, path_to_user)
+except IOError:
+    logging.info('No .fate directory is present.')
+else:
+    # NOTE: How to not except ImportErrors from the imported script?
+    import user
+    #try:
+        #import user
+        #logging.info('User script loaded.')
+    #except ImportError:
+        #logging.info('No user script is present in .fate directory.')
+
