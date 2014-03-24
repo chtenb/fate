01f6a994dc7c35ff05659e415d538e60c4b1d235
Chiel92 <ctje92@gmail.com>
2014-03-24 16:26:14 +0100

    add file now :)

---
+++ interactionstack.py
@@ -0,0 +1,28 @@
+"""A class definition for the clipboard and some actors."""
+from .action import Interactive
+
+class InteractionStack:
+    """Stack-like datastructure for storing and handling interactive actions."""
+    storage = []
+
+    @property
+    def isempty(self):
+        return len(self.storage) == 0
+
+    def push(self, interaction):
+        """Push interaction on the stack."""
+        assert isinstance(interaction, Interactive)
+        self.storage.append(interaction)
+
+    def peek(self, offset=0):
+        """
+        Return the interactoin with offset relative to the top of the stack.
+        """
+        return self.storage[-1 - offset]
+
+    def backtrack(self):
+        """Remove current interaction and return parent."""
+        self.storage.pop()
+        if not self.isempty:
+            return self.storage[-1]
+
