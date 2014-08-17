b304430d28d056c18d870f936e7a2da82e8f6146
Chiel92 <ctje92@gmail.com>
2014-08-17 15:42:21 +0200

    reproducibility of random testcases now only based on seeds

--- test/test_random_actions.py
+++ test/test_random_actions.py
@@ -1,11 +1,9 @@
-from unittest import main
 from .basetestcase import BaseTestCase
 from .. import commands
 from random import choice
 from ..commandmode import publics
 from ..commandtools import execute
 from tempfile import gettempdir
-from sys import path
 from os import urandom
 import random
 from ..runtest import args
@@ -13,38 +11,60 @@ from .randomized_userinterface import RandomizedUserSimulator
 
 command_dict = publics(commands)
 command_names = list(command_dict.keys())
+# Sorting is needed to be able to reproduce a seeded random test case
+command_names.sort()
+
 
 class RandomizedActionTest(BaseTestCase):
+
     def setUp(self):
         self.create_userinterface = RandomizedUserSimulator
         BaseTestCase.setUp(self)
+        self.runs, self.commands_per_run = (5000, 50) if args.long else (500, 50)
 
     def test_random_commands(self):
         if args.no_randomized_tests:
             print('Skipping randomized tests')
+            return
+
+        if args.rerun:
+            self.run_last_testcase()
         else:
-            if args.rerun:
-                path.insert(0, gettempdir())
-                try:
-                    from last_test_batch_fate import seed, batch
-                except ImportError:
-                    raise Exception('Can\'t rerun batch: no previous batch exists.')
-                else:
-                    self.run_batch(seed, batch)
-            else:
-                runs, commands = (5000, 50) if args.long else (500, 50)
-                for run in range(runs):
-                    print('Run ' + str(run + 1))
+            self.run_new_testcases()
 
-                    # Make sure to create a new document for each run
-                    self.setUp()
-                    seed = urandom(10)
-                    batch = [self.get_random_command() for _ in range(commands)]
+    def run_last_testcase(self):
+        """Rerun last testcase based on seed."""
+        try:
+            with open(gettempdir() + '/last_test_seed_fate.tmp') as f:
+                seed = int(f.read())
+        except IOError:
+            raise Exception('Can\'t rerun batch: no previous batch exists.')
+        else:
+            self.run_seeded_testcase(seed)
 
-                    self.run_batch(seed, batch)
+    def run_seeded_testcase(self, seed):
+        """Run a testcase based on given seed."""
+        random.seed(seed)
+        batch = [self.get_random_command() for _ in range(self.commands_per_run)]
+        self.run_batch(seed, batch)
+
+    def run_new_testcases(self):
+        """Run newly generated testcases."""
+        for run in range(self.runs):
+            print('Run ' + str(run + 1))
+
+            # Make sure to create a new document for each run
+            self.setUp()
+
+            # Generate a new seed
+            seed = int.from_bytes(urandom(10), byteorder='big')
+            random.seed(seed)
+
+            batch = [self.get_random_command() for _ in range(self.commands_per_run)]
+            self.run_batch(seed, batch)
 
     def run_batch(self, seed, batch):
-        savefile = gettempdir() + '/last_test_batch_fate.py'
+        savefile = gettempdir() + '/last_test_seed_fate.tmp'
 
         if args.verbose:
             print('Saving run into ' + savefile)
@@ -53,16 +73,12 @@ class RandomizedActionTest(BaseTestCase):
             print('Selection: ' + str(self.document.selection))
 
         with open(savefile, 'w') as f:
-            f.write('seed = {}\n'.format(seed))
-            f.write('batch = {}\n'.format(batch))
+            f.write(str(seed))
 
-        random.seed(seed)
         for i, name in enumerate(batch):
             if args.verbose:
                 print(str(i + 1) + ': executing ' + name)
             execute(command_dict[name], self.document)
-
-        #print('Result:\n' + self.document.text)
 
     def get_random_command(self):
         while 1:
@@ -70,4 +86,3 @@ class RandomizedActionTest(BaseTestCase):
             if name not in ['quit_document', 'force_quit', 'open_document']:
                 break
         return name
-
