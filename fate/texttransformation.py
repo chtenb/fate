e1e68d54f581b0d418255b612d3b20b730d04195
Chiel ten Brinke <chiel.tenbrinke@intellimagic.com>
2016-09-02 09:58:56 +0200

    Improve textttransformation documentation.

--- fate/texttransformation.py
+++ fate/texttransformation.py
@@ -5,6 +5,13 @@ away information about the transformation and it is a waste of space. We only wa
 the difference between two texts as a transformation. This reduces a transformation to a set of
 substitutions within a text.
 
+Note that it is possible to define other kinds of transformations that are not fully
+expressable as a set of substitutions in the text. For instance, the transformation formed by
+reversing the text is not really expressable in terms of substitutions while keeping the
+information about which position was mapped to which other position. But since there is not a
+clear and relevant application for these kind of transformations, we restrict ourselves to
+transformations formed by substitutions.
+
 We want to be able to carry over selections from the original text to the image of the
 transformation. So a text transformation must also define some sort of mapping of intervals
 across the transformation. When we need to map individual positions, we can derive this from
@@ -15,133 +22,160 @@ and derive from that how intervals should be mapped, as we will see later on.
 
 ### Mathematical properties
 Suppose M is a function that maps positions, intervals and selections accross a texttransformation.
-The following should hold.
+Because only substitutions are allowed in this transformation, the following should hold.
 Let a,b be positions.
-1. a <= b  ==>  M(a) <= M(b) (order preserving)
-
+1. a <= b  <==>  M(a) <= M(b) (order preserving)
 Let (a,b) and (c,d) be intervals.
-2. b <= c  ==>  M(b) <= M(c) (order preserving, disjointness preserving)
+2. b <= c  <==>  M(b) <= M(c) (order preserving, disjointness preserving)
 
+For selections we can simply map its individual intervals and form the result by putting the
+resulting intervals in a selection. By the above properties, the resulting selection will
+consist of disjoint intervals, and thus be a valid selection.
 
+We cannot take this further, and simply map individual positions. This approach appears to be
+not expressive enough for what we want to achieve. For example, if some text is inserted at a
+certain position x, we want to be able to express that x maps to the interval containing this
+newly inserted text. This is not possible if x were to map to a single position.
+
+We will now list the possible mappings of intervals to their images in a way that they don't
+conflict with other possible disjoint intervals in the same transformation.
 
 ### Substitutions of length 1
-To investigate this aspect further, let us for now restrict ourselves to transformations of
-length at most 1. We can distinguish three cases here. We do not try to minimize the number of
-cases or make them mutually exclusive. Our aim is to make it clear how intervals should be
-mapped and evident that out approach is exhaustive.
+To investigate these transformations further, let us for now restrict ourselves to
+substitutions of length at most 1 and see what possibilities we have of mapping intervals
+affected by this substitution. We can distinguish three cases here. We do not try to minimize
+the number of cases or make them mutually exclusive. Our aim is to make it clear how intervals
+should be mapped and evident that our approach is exhaustive. We will list all possible ways of
+mapping intervals that do not immiately conflict with other possible substitutions.
 
 1. Deletion of a character.
     Suppose a character is deleted between position x and x+1. Then affected intervals
     should be mapped as follows.
-    [a,x] => [a,x]
-    [a,x+1] => [a,x]
-    [x,a] => [x,a-1]
-    [x+1,a] => [x,a-1]
+    [a,x] => [a,x], for a < x
+    [a,x+1] => [a,x], for a < x
+    [x,a] => [x,a-1], for x+1 < a
+    [x+1,a] => [x,a-1], for x+1 < a
+    [x,x] => [x,x]
     [x,x+1] => [x,x]
+    [x+1,x+1] => [x,x]
     [a,b] => [a,b-1], for a < x < x+1 < b
 
 2. Insertion of a character.
     Suppose a character is inserted at position x. Then affected intervals should be
     mapped as follows.
-    [a,x] => [a,x] or [a,x+1]
-    [x,a] => [x,a+1] or [x+1,a+1]
+    [a,x] => [a,x] or [a,x+1], for a < x
+    [x,a] => [x,a+1] or [x+1,a+1], for x < a
     [x,x] => [x,x] or [x,x+1] or [x+1,x+1]
-    [a,b] => { [a,x], [x+1,b+1] } or [a,b+1], for a < x < x+1 < b
+    [a,b] => { [a,x], [x+1,b+1] } or [a,b+1], for a < x < b
 
 3. Substitution of a character with another character.
     Suppose a character is substituted between position x and x+1.
-    Nothing really changes. So affected intervals should be mapped as follows.
-    [a,x] => [a,x]
-    [a,x+1] => [a,x+1]
-    [x,a] => [x,a]
-    [x+1,a] => [x+1,a]
-    [x,x+1] => [x,x+1]
-    [a,b] =>  [a,b], for a < x < x+1 < b
-
-For case 2, a choice must be made for all kind of affected intervals.  Note that for the
-inverse, the deletion becomes an insertion, and as such, again a choice must be made.  This
-choice should be made at creation time, since at execution time the context and semantics of a
-certain substitution is not known.
-
+    So affected intervals should be mapped as follows.
+    [a,x] => [a,x] or [a,x+1], for a < x
+    [a,x+1] => [a,x+1] or [a,x], for a < x
+    [x,a] => [x,a] or [x+1,a], for x+1 < a
+    [x+1,a] => [x+1,a] or [x,a], for x+1 < a
+    [x,x] => [x,x] or [x,x+1] or [x+1,x+1]
+    [x,x+1] => [x,x], [x,x+1] or [x+1,x+1]
+    [x+1,x+1] => [x,x] or [x,x+1] or [x+1,x+1]
+    [a,b] => { [a,x], [x+1,b] } or [a,b], for a < x < x+1 < b
 
 ### Substitutions of arbitrary length
-We can distinguish three cases here.
+
 1. Deletion of a character.
     Suppose a sequence of characters is deleted between position x and x+s.
     Then affected intervals should be mapped as follows.
-    [a,x+y] => [a,x], for all 0 <= y <= s
-    [x+y,a] => [x,max(x,a-s)], for all 0 <= y <= s
-    [x,x+s] => [x,x]
+    [a,x+y] => [a,x], for all 0 <= y <= s, for a < x
+    [x+y,a] => [x,a-s], for all 0 <= y <= s, for x+s < a
+    any interval in [x,x+s] => [x,x]
     [a,b] => [a,b-s], for a < x < x+s < b
 
 2. Insertion of a character.
     Suppose a range of characters of length s is inserted at position x. Then affected
     intervals should be mapped as follows.
-    [a,x] => [a,x] or [a,x+s]
-    [x,a] => [x,a+s] or [x+s,a+s]
-    [x,x] => [x,x] or [x,x+s] or [x+s,x+s]
+    [a,x] => [a,x] or [a,x+s] or some in between, for a < x
+    [x,a] => [x,a+s] or [x+s,a+s] or some in between for x < a
+    [x,x] => some interval in [x,x+s]
     [a,b] => { [a,x], [x+s,b+s] } or [a,b+s], for a < x < x+s < b
 
 3. Substitution of a character with another character.
     Suppose the characters between position x and x+s are replaced with t characters,
-    0 < t and s < 0.  Then affected intervals should be mapped as follows.
-    [x,a] => [x,a-s+t]
-    [a,x+s] => [a,x+t]
-    [a,x+y] => [a,x] or [a,x+t], for all 1 <= y <= s-1
-    [x+y,a] => [x,a-s+t] or [x+t,a-s+t], for all 1 <= y <= s-1
-    [x,x+s] => [x,x+t]
+    0 < s,t.  Then affected intervals should be mapped as follows.
+    [a,x+y] => [a,x+z], for some 0 <= z <= s, for all 0 <= y <= s
+    [x+y,a] => [x+z,a], for some 0 <= z <= s, for all 0 <= y <= s
+    some interval in [x,x+s] => some interval in [x,x+t]
     [a,b] => { [a,x], [x+s,b] } or [a,b], for a < x < x+s < b
 
-Why do we only allow ambiguous interval end points to be mapped to endpoints of the
-substitution? Because we cannot know of any relation between characters inside the
-substitution operands. If some substitution wanted to expose such information, the
-substitution should be chopped down into finer substitutions such that related characters
-or substrings are exactly mapped onto each other.
+### Making choices in the mapping
 
-Again we see that choice has to be made in some situations. Let's do it.
+Sometimes a choice must be made between different images. This is not the case for deletions,
+but note that for the inverse, the deletion becomes an insertion, and as such, again a choice
+must be made.  This choice should probably be made at creation time, since at execution time the context
+and semantics of a certain substitution may not be known.
+
+It can be observed that not all combinations of choices satisfy mathematical property 2. We
+want to rule out options that cause this property to be violated.
+
+Moreover, we want to rule out all mappings that relate positions to other position that are
+strictly inside the interval image.  This is because we cannot know of any relation between
+characters inside the substitution operands. If some substitution wanted to expose such
+information, the substitution should be chopped down into finer substitutions such that related
+characters or substrings are exactly mapped onto each other.
+
+This results in the following mapping templates.
+
 For insertions we want to map
-[x,x] => [x,x+s]
 [a,x] => [a,x]
 [x,a] => [x+s,a+s]
-since otherwise intervals can end up being overlapping each other, which we want to avoid.
-I.e. we want intervals that are disjoint to remain disjoint after being taken accross the
-mapping.
-For substitutions we want to map
-[a,x+y] => [a,x], for all 1 <= y <= s-1, a <= x
-[x+y,a] => [x+t,a-s+t], for all 1 <= y <= s-1, x <= a
-[x+y,x+z] => [x,x], for all 1 <= y <= z <= s-1
-for the same reason.
+[x,x] => [x,x+s] or [x,x], depending on the type of insertion
+[a,b] => [a,b+s] or { [a,x], [x+s,b+s] }, depending on the type of insertion
+since otherwise intervals can end up being overlapping each other, which violates mathematical
+property 2. Furthermore, we choose [x,x] over [x+s,x+s] for no clear reason.
 
+By similar reasoning for substitutions we want to map
+[a,x+y] => [a,x], for all 0 <= y <= s-1
+[a,x+s] => [a,x+t] or [a,x], depending on the type of substitution
+[x+y,a] => [x+t,a-s+t], for all 1 <= y <= s
+[x,a] => [x,a-s+t] or [x+t,a-s+t], depending on the type of substitution
+some interval strictly in [x,x+s] => [x,x]
+[x,x+s] => [x,x+t] or [x,x], depending on the type of substitution
+[a,b] => [a,b+s] or { [a,x], [x+s,b+s] }, depending on the type of substitution
+
+Again we see that choice has to be made in some situations.
+So a special kind of substitution is to be introduced, which determines whether the first
+or the second options are chosen. Let's call the first option 'inclusive' and the second
+'exclusive'.
+
+--- TO BE CONTINUED HERE
 
 ### Multiple substitutions.
-All intervals should compensate for the intervals difference in size of the interval before
-them. Adjacent substitutions shouldn't complicate this much. The position that the two
-substitutions have in common should be mapped to the same image anyway.  If there are
-overlapping substitutions available on the same text, it is unclear what the textual result
-should be. So we pick the biggest or we just forbid it.  What if multiple insertions are done
-at the same position? It is not really clear if this falls under overlapping substitutions. We
-can either merge them to be a single insertion or forbid them.
+All mapped intervals should compensate for the positional difference caused by the
+substitutions before them.
+- If there are overlapping substitutions available on the same text, it is unclear what the
+  textual result should be. So we forbid it.
+- Adjacent substitutions complicate a little bit. Instead of having two candidate images,
+  the common position has three candidates.
+  Let x <= y <= z. Suppose [x,y] and [y,z] are substituted inducing the map M.
+  If both substitutions are inclusive, y is always mapped to M(y).
+  If both substitutions are exclusive, y is always mapped to M(x) or M(z).
+  If [x,y] is substituted exclusively and [y,z] inclusively, y is mapped to M(y) if its part of
+  an interval of the form [y,a]. If it is part of an interval of the form [a,y], y is mapped to
+  M(x). And [y,y] should be mapped to [M(y), M(y)].
+- What if multiple insertions are done at the same position x? The interval [x,x] is mapped to
+  the selection containing all images of the insertions, taking the exclusiveness into account
+  of course. All other intervals have nothing to do with this.
+- Sometimes a specific ordering is required. E.g. an insertion on a new line together with a
+  line number; the line number must go first. We must be able to express this. The ordering of
+  the selection and the replacements when creating a TextTransformation, must be maintained.
 
 
-### Composition of transformations.
-We don't need to be able to construct a single transformation from a chain of transformations.
-We can just apply the mappings and substitutions sequentially when we need to take something
-accross multiple transformations.
-
-
-### Inversion of transformations. What are the algebraic properties?
+### Inversion of transformations.
 It is very convenient for transformations to be fully invertible, in the sense that applying
 and undoing any transformation always ends up with the exact same situation as before.
 It is clear how we should invert insertion and replacement of a single character. What
 about deletion?  A deletion is not invertible, since two positions are merged into one.
 For any interval having at least one of these two positions as end point, it will be
 unclear how to invert the mapping without extra information.
-
-
-### How to create a transformation between two texts?
-E.g. for after formatting by external programs.  There exist several difference algorithms
-for this, each with its own downsides.  We don't need to have this in the core at the
-moment, so we can leave this be.
 
 
 ### Storing the mapping efficiently for speed and storage.
@@ -155,16 +189,39 @@ straight away. The position at an insertion should be mapped to two other positi
 should be treated special. The inner positions of a substitutions of length > 1 should
 also be mapped to two other positions, and be treated special as well.
 
+An observation we make is that each position inside a substitution can map two either of two
+candidate images, depending on the interval that is mapped and the type of substitution.
+So we can map each position outside a substitution to a single image position, and each
+position inside a substitution to a tuple of two positions, namely the two endpoints of the
+replacement. There remains one thing to be described in the representation, namely whether a
+substitution is inclusive or exclusive. We could subclass tuple and add a field which indicates
+this. We also need an indication for whether such a tuple was caused by an insertion or not.
+Although the latter can also be verified be checking this tuple has no similar neighbors.
+The same holds for indicating if such a tuple is an endpoint, and which.
+
+For storing multiple interval replacements at one position, we can simply use a list. This also
+maintains information about the ordering of the insertions, which is required.
+
+
+### Composition of transformations.
+We don't need to be able to construct a single transformation from a chain of transformations.
+We can just apply the mappings and substitutions sequentially when we need to take something
+accross multiple transformations.
+
+
+
+### How to create a transformation between two texts?
+E.g. for after formatting by external programs.  There exist several difference algorithms
+for this, each with its own downsides.  We don't need to have this in the core at the
+moment, so we can leave this be.
+
+
 ### Transforming parts of a text
 It should be easy to only transform a part of a text. Restrict all substitutions to one
 interval in the text. For the mapping this means that all positions before that interval are
 mapped to them selves and all positions after the interval are compensated for the difference
 in size of the transformed part of the text.
 
-###TODO
-Be able to apply text trasnformations on part of texts (i.e. just the part the is on the
-screen) for performance reasons. This is closely related to implementing a faster text
-datastructure for large texts.
 """
 
 from typing import List, Any
@@ -190,6 +247,25 @@ class IntervalSubstitution:
     @property
     def is_insertion(self):
         return self.interval.isempty
+
+
+class PositionReplacement(Interval):
+
+    def __new__(cls, beg, end, exclusive=False):
+        """
+        :beg: the beg of the interval that envelops this position replacement
+        :end: the end of the interval that envelops this position replacement
+        :exclusive: whether the substitution was exclusive. Default is inclusive.
+        """
+        tuple.__new__(cls, beg, end)
+
+    def __init__(self, beg, end, exclusive=False):
+        self.exclusive = exclusive
+
+    def __repr__(self):
+        if not self.exclusive:
+            return Interval.__repr__(self)
+        return 'excl' + Interval.__repr__(self)
 
 
 class IntervalMapping:
@@ -435,4 +511,3 @@ class PartialTextTransformation(TextTransformation):
         selection = Selection(intervals)
 
         TextTransformation.__init__(self, selection, replacements, text)
-
